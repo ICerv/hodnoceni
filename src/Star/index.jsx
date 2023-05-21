faf03358cbb4f963a7f91789ff6f29f9f15d7b3c
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-05-21 14:52:54 +0200

    Task completed

--- src/Star/index.jsx
+++ src/Star/index.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import './style.css';
 
-const Star = () => {
+const Star = ({ onGlow, onDim }) => {
   const [glowing, setGlowing] = useState(false);
-  
+
   const starClass = glowing
     ? 'rating__star rating__star--on'
     : 'rating__star';
 
+  const handleOnClick = () => {
+    const newGlowing = !glowing;
+    setGlowing(newGlowing);
+    if (newGlowing) {
+      onGlow();
+    } else {
+      onDim();
+    }
+  }
+
   return (
-    <div className={starClass}></div>
+    <div onClick={handleOnClick} className={starClass}></div>
   );
 };
 
