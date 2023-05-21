f6eff4a395218df043a118b51cab3715a7d943b8
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-05-21 15:03:01 +0200

    Task2 completed

--- src/Rating/index.jsx
+++ src/Rating/index.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import Star from '../Star';
 import './style.css';
 
+
+// HODNOCENI 1.
+/*
 const Rating = () => {
   const [value, setValue] = useState(0);
 
@@ -24,6 +27,31 @@ const Rating = () => {
         <Star onGlow={handleGlow} onDim={handleDim} />
         <Star onGlow={handleGlow} onDim={handleDim} />
         <Star onGlow={handleGlow} onDim={handleDim} />
+      </div>
+    </div>
+  );
+};
+
+export default Rating;
+*/
+// HODNOCENI 2.
+
+const Rating = () => {
+  const [value, setValue] = useState(0);
+
+  const handleSelect = (rating) => {
+    setValue(rating);
+  };
+
+  return (
+    <div className="rating">
+      <div className="rating__value">{value}</div>
+      <div className="rating__stars">
+        <Star glowing={value >= 1} rating={1} onSelect={handleSelect} />
+        <Star glowing={value >= 2} rating={2} onSelect={handleSelect} />
+        <Star glowing={value >= 3} rating={3} onSelect={handleSelect} />
+        <Star glowing={value >= 4} rating={4} onSelect={handleSelect} />
+        <Star glowing={value >= 5} rating={5} onSelect={handleSelect} />
       </div>
     </div>
   );
--- src/Star/index.jsx
+++ src/Star/index.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './style.css';
 
+
+// HODNOCENI 1.
+/*
 const Star = ({ onGlow, onDim }) => {
   const [glowing, setGlowing] = useState(false);
 
@@ -17,6 +20,22 @@ const Star = ({ onGlow, onDim }) => {
       onDim();
     }
   }
+
+  return (
+    <div onClick={handleOnClick} className={starClass}></div>
+  );
+};
+
+export default Star;
+*/
+// HODNOCENI 2.
+
+const Star = ({ glowing, rating, onSelect }) => {
+  const starClass = glowing ? 'rating__star rating__star--on' : 'rating__star';
+
+  const handleOnClick = () => {
+    onSelect(rating);
+  };
 
   return (
     <div onClick={handleOnClick} className={starClass}></div>
